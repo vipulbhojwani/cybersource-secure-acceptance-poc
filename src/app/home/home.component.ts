af8fcf0557c0739f8bfb7337019a9de63ea9b8de
Vipul Bhojwani <vipul.bhojwani@pearson.com>
2020-01-24 18:46:51 +0530

    updating

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
     transaction_uuid: uuid.v4(),    
     signed_field_names: 'access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency',
     unsigned_field_names: '',
-    signed_date_time: moment().utc().format('YYYY-MM-DD[T]hh:mm:ss[Z]'),
+    signed_date_time: moment().utc().format('YYYY-MM-DD[T]HH:mm:ss[Z]'),
     locale: 'en',
     transaction_type: 'authorization',
     reference_number: new Date().getTime(),
