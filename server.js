cb3781b35cd169e0a8e311a5a4875b2850a49cb5
Vipul Bhojwani <vipul.bhojwani@pearson.com>
2020-01-24 16:26:45 +0530

    updating

--- server.js
+++ server.js
@@ -1,56 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var path = require('path');
 
+console.log("1");
+var express = require('express');
+console.log("2");
+
+console.log("3");
+
+console.log("4");
+
+console.log("5");
+
+
+var path = require('path')
 
 var indexRouter = require('./server/routes/index');
+
+console.log("6");
+
 //var cyberSourceRouter = require('./server/routes/cybersource');
 
 var cors = require('cors')
 
+
+console.log("7");
+
 var port = process.env.PORT || 8080;
 
 
+console.log("8");
 
 var app = express();
 
+console.log("9");
+
 app.use(cors())
 
+console.log("10");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/dist/app')));
+
+
+console.log("11");
 
 app.use('/encode', indexRouter);
 //app.use('/cybersource', cyberSourceRouter);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+console.log("12");
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
+
+console.log(port);
 app.listen(port);
 
 
-module.exports = app;
+
